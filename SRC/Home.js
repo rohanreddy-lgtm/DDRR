6931fa70b4b14fdbbadf96ab3577af6b6655971e
Shankuri Rohanreddy <shankurirohan76@gmail.com>
2025-02-14 14:49:34 +0530

    no

---
+++ SRC/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Welcome to My Home Page</h1>
+      <p>
+        <strong>Name:</strong> Your Name
+      </p>
+      <p>
+        <strong>Contact:</strong> your.email@example.com
+      </p>
+    </div>
+  );
+};
+
+export default Home;
